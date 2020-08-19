124f94f27663744d5045502b8056a76a9fff6e0f
kamranM-dev <kamranmedjidov7@gmail.com>
2020-08-19 22:39:06 +0300

    first commit

---
+++ build/precache-manifest.1613ffb65d7a0a63a52a1342419630f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a49bc96e42d9989e1325dfd7f3f5d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fdc43f6a56087f0fd1f",
+    "url": "/static/css/main.ed85e40f.chunk.css"
+  },
+  {
+    "revision": "5acd30c92458b565631f",
+    "url": "/static/js/2.a3aa4d71.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a3aa4d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdc43f6a56087f0fd1f",
+    "url": "/static/js/main.da2ba048.chunk.js"
+  },
+  {
+    "revision": "d12b5a4787fca4e6d9ea",
+    "url": "/static/js/runtime-main.afb7cc16.js"
+  }
+]);
